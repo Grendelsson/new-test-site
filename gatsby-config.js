ed61a848bef98dbc0c9abf8c9b1e14bf5fa6a317
Grendelsson <spamcabbagespam@hotmail.com>
2022-04-08 13:10:44 +0000

    Get sub-pages working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  pathPrefix: "/new-test-site",
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Nonsense site full of rubbish.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
