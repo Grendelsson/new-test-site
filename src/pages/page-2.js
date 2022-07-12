e3c6da2638bafa90750b31aa61db494e86c696f8
Grendelsson <spamcabbagespam@hotmail.com>
2022-07-12 15:21:39 +0000

    Hopefully working listener

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,15 +4,27 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Put some stuff here</h1>
-    <p>Generate some content to put here. Anything really.</p>
-    <iframe src="https://grendelsson.github.io/content/test.html">
-    </iframe>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
 
-export default SecondPage
+export default function SecondPage(){
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.dataLayer = window.dataLayer || [];
+      var eventMethod = window.addEventListener? "addEventListener" : "attachEvent";
+      var eventer = window[eventMethod];
+      var messageEvent = eventMethod === "attachEvent"? "onmessage" : "message";
+      eventer(messageEvent, function (e) {
+      //data you receive from parent is stored inside e.data
+          window.dataLayer.push(e.data);
+      });
+  }
+  }, []);
+  return (
+    <Layout>
+      <Seo title="Page two" />
+      <h1>Put some stuff here</h1>
+      <p>Generate some content to put here. Anything really.</p>
+      <iframe src="https://grendelsson.github.io/content/test.html" title="iframe_for_test">
+      </iframe>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+)};
