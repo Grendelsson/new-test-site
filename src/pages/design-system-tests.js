598f46f09da6934fbc5cd239ffcfebf620fc5212
Grendelsson <spamcabbagespam@hotmail.com>
2022-11-02 15:23:23 +0000

    publish

--- src/pages/design-system-tests.js
+++ src/pages/design-system-tests.js
@@ -3,19 +3,18 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-
 const DesignSystemTest = () => (
   <Layout>
     <Seo title="404: Not found" />
     <nav class="stacked-nav" data-component="stacked-nav">
         <ul class="stacked-nav__list">
             <li class="stacked-nav__list-item">
-            <a aria-current="false" data-level={1} data-text="top level" href="#">
+            <a aria-current="false" data-level={1} data-text="top level" href="#1">
                 <span class="stacked-nav__content-wrapper">Top level</span>
             </a>
             <ul class="stacked-nav__nested">
                 <li class="stacked-nav__list-item">
-                <a aria-current="false" data-level={2} data-text="nested link one" href="#">
+                <a aria-current="false" data-level={2} data-text="nested link one" href="#2">
                     <span class="stacked-nav__content-wrapper">
                     Nested link one
                     <span class="stacked-nav__hint">hint text</span>
@@ -23,12 +22,12 @@ const DesignSystemTest = () => (
                 </a>
                 </li>
                 <li class="stacked-nav__list-item">
-                <a aria-current="false" data-level={2} data-text="nested link two" href="#">
+                <a aria-current="false" data-level={2} data-text="nested link two" href="#3">
                     <span class="stacked-nav__content-wrapper">Nested link two</span>
                 </a>
                 <ul class="stacked-nav__nested">
                     <li class="stacked-nav__list-item">
-                    <a aria-current="false" data-level={3} data-text="deep nested link" href="#">
+                    <a aria-current="false" data-level={3} data-text="deep nested link" href="#4">
                         <span class="stacked-nav__content-wrapper">
                         Deep nested link
                         <span class="stacked-nav__hint">hint text</span>
