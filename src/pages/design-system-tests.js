cfd02defd43ae9ecd41e46f775d6709ca28a84fe
Grendelsson <spamcabbagespam@hotmail.com>
2022-11-04 13:14:59 +0000

    publish

--- src/pages/design-system-tests.js
+++ src/pages/design-system-tests.js
@@ -5,21 +5,33 @@ import Seo from "../components/seo"
 import { StackedNav, StackedNavLink } from "@nice-digital/nds-stacked-nav"
 import { Link } from "gatsby"
 
+const setOfLinksToNest = (
+  <>
+		<StackedNavLink hint="hint text" to="#link-one">Nested link one</StackedNavLink>
+		<StackedNavLink to="#link-two"
+			nested={
+				<StackedNavLink hint="hint text" to="#deep">Deep nested link</StackedNavLink>
+			}
+		>
+			Nested link two
+		</StackedNavLink>
+  </>
+);
+
 const DesignSystemTest = () => (
   <Layout>
     <Seo title="Design System tracking tests" />
-    <StackedNav label="Explore our site" elementType="h2">
-        <StackedNavLink to="#about">About Page</StackedNavLink>
-        <StackedNavLink to="#contact">Contact Page</StackedNavLink>
-    </StackedNav>
-
     <StackedNav
         label="Explore our site"
         elementType="h3"
         link={{ destination: "#sitemap", isCurrent: true, elementType: Link }}
-    >
+        >
+      <StackedNavLink to="#" nested={setOfLinksToNest}>
+        Stacked nav stuff 
+    </StackedNavLink>
         <StackedNavLink to="#about" label="About Page"/>
         <StackedNavLink to="#contact" label="Contact Page"/>
+        
     </StackedNav>
   </Layout>
 )
